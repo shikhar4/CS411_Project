557922e729320df7c7c54d6e15c520976c9db85c
Dyshant <dyshu820@gmail.com>
2020-11-02 23:06:51 -0600

    server code

--- server/index.js
+++ server/index.js
@@ -2,18 +2,24 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'localborrowme'
-})
-app.get("/",(req,res)=>{
-    const sqlInsert = "INSESRT INTO user (user_id, password,phoneNumber,email,address,score,friendsList) VALUES ('2','5678','4087754635','shik4@illinois.edu','1234 Beast Ave.','999','anchit');"
-    db.query(sqlInsert,(err,result)=>{
-        res.send("hello test");
-    });
-    
-})
+const con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "password"
+});
 
+con.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+  
+  app.get("/",(req,res) =>
+  {
+      
+      res.send("shik you suck");
+  })
 app.listen(3001, ()=> {console.log("running on 3001");});
