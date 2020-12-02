5234531c699a9d15efec7ac8253c2dc8f25766f9
shikhar41 <shikhar4@illinois.edu>
2020-12-01 18:22:52 -0800

    Friends Table Button working

--- client/src/Home.js
+++ client/src/Home.js
@@ -13,8 +13,11 @@ const user_id = localStorage.getItem("user_id_global")
 var product_table =[];
 var table_data = []
 
-var friend_table = []
+var friend_table_data = []
 var friend_head = []
+
+var friend_list_strings = ""
+
 var head = []
 
 
@@ -24,6 +27,7 @@ class Home extends Component {
        super(props);
        this.state={
          product_table:[],
+         friend_table:[],
          showInsertComponent: false,
          showDeleteComponent: false
        }
@@ -34,6 +38,7 @@ class Home extends Component {
        this.loadInsertTable = this.loadInsertTable.bind(this)
        this.loadDeleteTable = this.loadDeleteTable.bind(this)
        this.getFriendData = this.getFriendData.bind(this)
+       this.add_friend = this.add_friend.bind(this)
       //  this.add_entry = this.add_entry(this);
    }
 
@@ -44,24 +49,97 @@ class Home extends Component {
         product_table : [...prevState.product_table, product_arr]
       }))
   }
+  add_friend(UserName, FirstName, LastName, zipCode){
+    //console.log(friendID)
+    const friend_arr = {UserName, FirstName, LastName, zipCode}
+      this.setState(prevState =>({
+        friend_table : [...prevState.friend_table, friend_arr]
+      }))
+  }
 
   getFriendData(){ 
+    document.getElementById("Friends_Table").style.visibility = "visible";
+    this.setState({showInsertComponent : false})
+    this.setState({showDeleteComponent : false})
+    document.getElementById("Items_Table").style.visibility = "collapse"
+    friend_head = [] 
+    friend_table_data = []
+    this.setState({friend_table: []})
+    
     Axios.post('http://localhost:3001/mongo/find',
     {userID:localStorage.getItem("user_id_global")}).then((res)=>{
        // console.log(res.data[0].friend_list)
-        console.log(res.data[0].friend_list[0])
+        //console.log(res.data[0].friend_list[0])
+        
         
         for(var i = 0; i < res.data[0].friend_list.length; i++)
         { 
-          console.log(res.data[0].friend_list[i])
-          friend_table.push(res.data[0].friend_list[i])
+          //console.log(res.data[0].friend_list[i])
+          var x = res.data[0].friend_list[i]
+          friend_list_strings = friend_list_strings +" "
+          friend_list_strings = friend_list_strings + JSON.stringify(x);
           
         }
+
       
     
       })
-    console.log(friend_table)
+    //console.log(this.state.friend_table)
+    console.log(friend_list_strings)
+    var friends1 = friend_list_strings.split(' ')
+    friend_list_strings = "";
+
+    console.log("FRIENDS1", friends1.length)
+    
+    //console.log(friend_table[0])
+    //console.log(friend_table[1])
+
+    var friendPromiseArr = [];
+    // if(this.state.friend_table.length > 0){
+    if(friends1.length > 0){
+      for(var i = 1; i < friends1.length; i++){
+        
+        friendPromiseArr.push(Axios.post('http://localhost:3001/api/get_friend_information',
+        {friendID: friends1[i]})
+        )
+      }
+
+      Promise.all(friendPromiseArr).then(resp=>{
+        for(var i = 0; i <resp.length; i++){
+          
+          this.add_friend(resp[i].data[0].UserName, resp[i].data[0].FirstName, resp[i].data[0].LastName, resp[i].data[0].zipCode)
+        }
+        
+      }).catch(err => {console.log(err)})
+
+      console.log("FRIEND TABLE DATA", this.state.friend_table); 
+
+      if(this.state.friend_table.length > 0){
+        friend_head.push(<th key={'UserName'}>{'UserName'}</th>)
+        friend_head.push(<th key={'FirstName'}>{'First Name'}</th>)
+        friend_head.push(<th key={'LastName'}>{'Last Name'}</th>)
+        friend_head.push(<th key={'zipCode'}>{'Zipcode'}</th>)
+      
+  
+  
+        for(var i = 0; i < this.state.friend_table.length; i++){
+          const {UserName, FirstName, LastName, zipCode}= this.state.friend_table[i]
+          console.log(UserName, FirstName, LastName, zipCode)
+          friend_table_data.push(
+            <tr>
+              <td>{UserName}</td>
+              <td>{FirstName}</td>
+              <td>{LastName}</td>
+              <td>{zipCode}</td>
+            </tr>)
+        }
+        
+      }
+    }
   }
+    
+
+  
 
   refreshTable(){
     head = [] 
@@ -69,6 +147,7 @@ class Home extends Component {
     document.getElementById("Items_Table").style.visibility = "visible";
     this.setState({showInsertComponent : false})
     this.setState({showDeleteComponent : false})
+    document.getElementById("Friends_Table").style.visibility = "hidden"
     console.log(this.state.showInsertComponent)
     Axios.post('http://localhost:3001/api/search',
     {userID:localStorage.getItem("user_id_global")}).then((res)=>{
@@ -120,6 +199,7 @@ class Home extends Component {
    }
    loadInsertTable(){
      document.getElementById("Items_Table").style.visibility = "hidden";
+     document.getElementById("Friends_Table").style.visibility = "hidden"
      this.setState({showDeleteComponent:false})
 
      this.setState({showInsertComponent:true})
@@ -128,6 +208,7 @@ class Home extends Component {
    }
    loadDeleteTable(){
      document.getElementById("Items_Table").style.visibility = "hidden"
+     document.getElementById("Friends_Table").style.visibility = "hidden"
      this.setState({showInsertComponent:false})
 
      this.setState({showDeleteComponent:true})
@@ -156,6 +237,18 @@ class Home extends Component {
         </thead>
         <tbody>
             {table_data}  
+        </tbody>
+        </table>
+      </div>
+      <div>
+        <table className = "table table-hover" id = "Friends_Table">
+        <thead>
+            <tr>
+            {friend_head}
+            </tr>
+        </thead>
+        <tbody>
+            {friend_table_data}  
         </tbody>
         </table>
       </div>
